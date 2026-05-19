2a35074811fee8ec7bc094c5a5357278bb07a485
elija-xp <eyelja@gmail.com>
2026-05-20 01:00:52 +0300

    implement driver forms, license validation, and car assignment

---
+++ taxi/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Driver, Car
+
+
+class DriverCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + (
+            "license_number",
+            "first_name",
+            "last_name"
+        )
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ("license_number",)
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=Driver.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
+
+
+def validate_license_number(license_number):
+    if len(license_number) != 8:
+        raise forms.ValidationError(
+            "License number must consist"
+            " of exactly 8 characters"
+        )
+    if not license_number[:3].isupper() or not license_number[:3].isalpha():
+        raise forms.ValidationError(
+            "First 3 characters must"
+            " be uppercase letters"
+        )
+    if not license_number[3:].isdigit():
+        raise forms.ValidationError("Last 5 characters must be digits")
+    return license_number
